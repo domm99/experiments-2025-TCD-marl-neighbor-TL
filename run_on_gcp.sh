e426b38fd71b1e1cbd8e9e1f063c42c817da9e6b
davidedomini <davide.domini@unibo.it>
2025-11-26 08:34:04 +0000

    chore: add script to run experiemnts on GCP

---
+++ run_on_gcp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PARAMS=(
+	"--max_seed 1 --transfer_enabled False --restricted_communication False --env_name flocking"
+	"--max_seed 1 --transfer_enabled True --restricted_communication False --env_name flocking"
+	"--max_seed 1 --transfer_enabled True --restricted_communication True --env_name flocking"
+)
+
+LOG_FILE="LOG.log"
+FINISHED_EXPERIMENTS_FILE = "experiments.txt"
+
+
+for param in "${PARAMS[@]}"; do
+
+    xvfb-run -a -s "-screen 0 1400x900x24" \
+    poetry run python3 src/main.py $param >> "$LOG_FILE" 2>&1
+
+    echo "Experiment $param completed" >> "$FINISHED_EXPERIMENTS_FILE"
+
+done
